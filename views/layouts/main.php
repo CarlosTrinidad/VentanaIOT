023efd5198b8a35194d6dfb2a9460be5ea90e71b
CristianDoGo <arturo_cristian@hotmail.com>
2016-12-02 03:04:56 -0600

    Se creo el CRUD User, se crearon las funciones para validar el login, se modificaron los forms para el password, se agrego al menutop Usuarios

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,21 +38,17 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Alarmas', 'url' => ['/alarmas/index']],
             ['label' => 'Graficas', 'url' => ['/window/index']],
+            ['label' => 'Usuarios', 'url' => ['/user/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
-            
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+
+            Yii::$app->user->isGuest ? 
+                ['label' => 'Login', 'url' => ['/site/login']]:
+                [
+                        'label' => 'Logout (' . Yii::$app->user->identity->user_name . ')',
+                        'url' => ['/site/logout'],
+                        'linkOptions' => ['data-method' => 'post']
+                    ],
         ],
     ]);
     NavBar::end();
